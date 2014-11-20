4c08d7cfd27d938d19b6b29851725587e00e4c20
Kevin Wu <kevinpingwu2@gmail.com>
2014-11-20 19:58:04 +1100

    added progress view but not wired

---
+++ cicada/AccommodationRequestPV.swift
@@ -0,0 +1,22 @@
+//
+//  AccommodationRequestPV.swift
+//  cicada
+//
+//  Created by Ping on 20/11/2014.
+//  Copyright (c) 2014 Yang Ltd. All rights reserved.
+//
+
+import UIKit
+
+class AccommodationRequestPV: RequestProgressView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect)
+    {
+        // Drawing code
+    }
+    */
+
+}
