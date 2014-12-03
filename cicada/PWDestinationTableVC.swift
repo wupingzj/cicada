739e1bbf2ba253e1bb1c1bf3aeb67f1807f08c90
Kevin Wu <kevinpingwu2@gmail.com>
2014-12-03 22:41:40 +1100

    Added section title index on the right side. However, the searchBar is no longer working

--- cicada/PWDestinationTableVC.swift
+++ cicada/PWDestinationTableVC.swift
@@ -9,8 +9,13 @@
 import UIKit
 
 class PWDestinationTableVC: UITableViewController, UISearchBarDelegate {
-    var tableData: [String] = ["One1","Two1", "Three1", "One2","Two2", "Three2", "One3","Two3", "Three3", "One4","Two", "Three", "One5","Two", "Three", "One3","Two", "Three", "One","Two", "Three", "One","Two", "Three", "One","Two", "Three", "One4","Two", "Three", "One","Two", "Three", "One","Two", "Three", "One","Two", "Three", "One5","Two", "Three", "One","Two", "Three", "One","Two", "Three", "One","Two", "Three"]
+//    var tableData: [String] = ["One1","Two1", "Three1", "One2","Two2", "Three2", "One3","Two3", "Three3", "One4","Two", "Three", "One5","Two", "Three", "One3","Two", "Three", "One","Two", "Three", "One","Two", "Three", "One","Two", "Three", "One4","Two", "Three", "One","Two", "Three", "One","Two", "Three", "One","Two", "Three", "One5","Two", "Three", "One","Two", "Three", "One","Two", "Three", "One","Two", "Three"]
     var searchResult: [String] =  [String]()
+    
+    
+    var tableData: Dictionary<String, [String]> = ["B":["bear", "bee"], "C":["cat","cow"], "D":["Dog"], "E":["Emu"], "F":["frog"], "H":["horse"], "Y":["yak"]]
+    
+    var sectionTitles: [String] = [String]()
     
     @IBOutlet var infoView: UIView!
     @IBOutlet var searchBar: UISearchBar!
@@ -25,7 +30,10 @@ class PWDestinationTableVC: UITableViewController, UISearchBarDelegate {
         
         countryButton.setTitle("Australia", forState: UIControlState.Normal)
         
-        addMySeparatorLine()
+        //addMySeparatorLine()
+        
+        sectionTitles = tableData.keys.array
+        sectionTitles.sort() {$0 < $1}
     }
     
     func addMySeparatorLine() {
@@ -39,14 +47,23 @@ class PWDestinationTableVC: UITableViewController, UISearchBarDelegate {
     // MARK: - Table view data source
     
     override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-        return 1
+        return sectionTitles.count
     }
     
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         if (isFiltered) {
+            // TODO
             return searchResult.count
         } else {
-            return tableData.count
+            //return tableData.count
+            let sectionTitle: String = sectionTitles[section]
+            let a: [String]? = tableData[sectionTitle]
+            if a == nil {
+                return 0
+            } else {
+                return a!.count
+            }
+            
         }
     }
     
@@ -54,12 +71,40 @@ class PWDestinationTableVC: UITableViewController, UISearchBarDelegate {
         let cell = tableView.dequeueReusableCellWithIdentifier("destinationCell", forIndexPath: indexPath) as UITableViewCell
         
         if (isFiltered) {
+            // TODO
             cell.textLabel!.text = searchResult[indexPath.row]
         } else {
-            cell.textLabel!.text = tableData[indexPath.row]
+            //cell.textLabel!.text = tableData[indexPath.row]
+            
+            //return tableData.count
+            var text = ""
+            let sectionTitle: String = sectionTitles[indexPath.section]
+            let a: [String]? = tableData[sectionTitle]
+            if a == nil {
+                text = "Not Found"
+            } else {
+                text = a![indexPath.row]
+            }
+            
+            cell.textLabel!.text = text
         }
         
         return cell
+    }
+    
+//    override func tableView(tableView: UITableView, sectionForSectionIndexTitle title: String, atIndex index: Int) -> Int {
+//        <#code#>
+//    }
+    
+    override func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        
+        return sectionTitles[section]
+    }
+    
+    // show Section Index Titles
+    // ref: http://www.appcoda.com/ios-programming-index-list-uitableview/
+    override func sectionIndexTitlesForTableView(tableView: UITableView) -> [AnyObject]! {
+        return sectionTitles
     }
     
     /*
@@ -97,9 +142,9 @@ class PWDestinationTableVC: UITableViewController, UISearchBarDelegate {
         return m.rangeOfString(searchText, options: NSStringCompareOptions.CaseInsensitiveSearch, range: nil, locale: nil) != nil
         }*/
         
-        searchResult = tableData.filter() {
-            $0.rangeOfString(searchText, options: NSStringCompareOptions.CaseInsensitiveSearch, range: nil, locale: nil) != nil
-        }
+//        searchResult = tableData.filter() {
+//            $0.rangeOfString(searchText, options: NSStringCompareOptions.CaseInsensitiveSearch, range: nil, locale: nil) != nil
+//        }
         
         println("searchText=\(searchText)     searchResult.count=\(searchResult.count)")
         
